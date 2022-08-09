ac05b6e37785b793b23c3670107a551691f56d48
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-08-09 18:15:20 +0600

    themes setup done

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <h1>Traveling Agency App</h1>
+      <h1 className="text-7xl bg-violet-700 text-white p-10">
+        Traveling Agency App
+      </h1>
       <button class="btn btn-secondary">Button</button>
     </div>
   );
-} 
+}
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,33 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        light: {
+          primary: "#0aff8d",
+          secondary: "#19d3ae",
+          accent: "#191919",
+          neutral: "#3D4451",
+          "base-100": "#FFFFFF",
+          info: "#82bd69",
+          success: "#3ABFF8",
+          warning: "#FBBD23",
+          error: "#F87272",
+        },
+        dark: {
+          primary: "#3ABFF8",
+          secondary: "#828DF8",
+          accent: "#F471B5",
+          neutral: "#1D283A",
+          "base-100": "#0b1120",
+          info: "#0CA6E9",
+          success: "#2BD4BD",
+          warning: "#F4C152",
+          error: "#FB6F84",
+        },
+      },
+    ],
+  },
   plugins: [require("daisyui")],
 };
