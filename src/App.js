38a1bf348827f8c2378ca6723fd1ee71c271eb04
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-08-09 21:37:01 +0600

    react-router install & setup done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
-    <div>
+    <section>
+      <Routes>
+        <Route path="/" element={""} />
+      </Routes>
       <h1 className="text-7xl bg-violet-700 text-white p-10">
         Traveling Agency App
       </h1>
-      <button class="btn btn-secondary">Button</button>
-    </div>
+      <button class="btn btn-primary">Button</button>
+    </section>
   );
 }
 
