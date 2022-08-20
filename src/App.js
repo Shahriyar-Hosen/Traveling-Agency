42a7b89d06f6b785a6607c481b735c6265c33224
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-08-20 20:54:35 +0600

    search feacher emolen t

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Footer from "./Components/Shared/Footer";
 import Navbar from "./Components/Shared/Navbar";
-import Home from "./Page/Home.jsx";
+import Home from "./Page/Home.js";
 
 function App() {
   return (
-    <section>
+    <section className="max-w-7xl mx-auto">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
--- src/Components/Shared/Navbar.js
+++ src/Components/Shared/Navbar.js
@@ -2,11 +2,56 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const navMenu = (
+    <>
+      <li>
+        <Link to="/">Menu 1</Link>
+      </li>
+      <li>
+        <Link to="/">Menu 2</Link>
+      </li>
+      <li>
+        <Link to="/">Menu 3</Link>
+      </li>
+    </>
+  );
   return (
-    <div class="navbar bg-base-100">
-      <div class="navbar-start">
-        <div class="dropdown">
-          <label tabindex="0" class="btn btn-ghost lg:hidden">
+    <>
+      <div class="navbar bg-base-100 px-3">
+        <div class="navbar-start">
+          <div class="dropdown">
+            <label tabindex="0" class="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabindex="0"
+              class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              {navMenu}
+            </ul>
+          </div>
+          <Link to="/" class="btn btn-ghost normal-case text-xl">
+            daisyUI
+          </Link>
+        </div>
+        <div class="navbar-center hidden lg:flex">
+          <ul class="menu menu-horizontal p-0">{navMenu}</ul>
+        </div>
+        <div class="navbar-end">
+        <button class="btn btn-ghost btn-circle">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               class="h-5 w-5"
@@ -18,86 +63,80 @@ const Navbar = () => {
                 stroke-linecap="round"
                 stroke-linejoin="round"
                 stroke-width="2"
-                d="M4 6h16M4 12h8m-8 6h16"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
               />
             </svg>
-          </label>
-          <ul
-            tabindex="0"
-            class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            <li>
-              <Link to="/">Item 1</Link>
-            </li>
-            <li tabindex="0">
-              <Link to="/" class="justify-between">
-                Parent
-                <svg
-                  class="fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
-                </svg>
-              </Link>
-              <ul class="p-2">
+          </button>
+          <button class="btn btn-ghost btn-circle">
+            <div class="indicator">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+                />
+              </svg>
+              <span class="badge badge-xs badge-primary indicator-item"></span>
+            </div>
+          </button>
+          <div class="flex justify-center items-center gap-2">
+            <div class="dropdown dropdown-end">
+              <label tabindex="0" class="btn btn-ghost btn-circle avatar">
+                <div class="w-10 rounded-full">
+                  <img src="https://placeimg.com/80/80/people" alt="" />
+                </div>
+              </label>
+              <ul
+                tabindex="0"
+                class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+              >
                 <li>
-                  <Link to="/">Submenu 1</Link>
+                  <Link to="/" class="justify-between">
+                    Profile
+                    <span class="badge">New</span>
+                  </Link>
                 </li>
                 <li>
-                  <Link to="/">Submenu 2</Link>
+                  <Link to="/">Settings</Link>
+                </li>
+                <li>
+                  <Link to="/">Logout</Link>
+                </li>
+                <li>
+                  <div class="indicator flex justify-center items-center gap-3">
+                    <h1>Card</h1>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="h-5 w-5"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                      />
+                    </svg>
+                    <span class="badge badge-sm indicator-item">8</span>
+                  </div>
                 </li>
               </ul>
-            </li>
-            <li>
-              <Link to="/">Item 3</Link>
-            </li>
-          </ul>
+            </div>
+          </div>
         </div>
-        <Link to="/" class="btn btn-ghost normal-case text-xl">
-          daisyUI
-        </Link>
       </div>
-      <div class="navbar-center hidden lg:flex">
-        <ul class="menu menu-horizontal p-0">
-          <li>
-            <Link to="/">Item 1</Link>
-          </li>
-          <li tabindex="0">
-            <Link to="/">
-              Parent
-              <svg
-                class="fill-current"
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                viewBox="0 0 24 24"
-              >
-                <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-              </svg>
-            </Link>
-            <ul class="p-2">
-              <li>
-                <Link to="/">Submenu 1</Link>
-              </li>
-              <li>
-                <Link to="/">Submenu 2</Link>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <Link to="/">Item 3</Link>
-          </li>
-        </ul>
-      </div>
-      <div class="navbar-end">
-        <Link to="/" class="btn btn-primary">
-          Get started
-        </Link>
-      </div>
-    </div>
+
+      
+    </>
   );
 };
 
