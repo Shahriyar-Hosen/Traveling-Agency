a34e1291f87dd8a1da63e37562aabdf8e7caaa79
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-09-03 12:20:36 +0600

    places

---
+++ src/Components/Home/Places.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Places = () => {
+  return (
+    <div>
+      <h1>There are mani places</h1>
+    </div>
+  );
+};
+
+export default Places;
--- src/Page/Home.js
+++ src/Page/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../Components/Home/Header";
+import Places from "../Components/Home/Places";
 
 const Home = () => {
   return (
     <div>
       <Header />
-      
+      <Places />
+
       <h1 className="text-7xl mt-10 bg-violet-700 text-white p-10">
         Traveling Agency App
       </h1>
