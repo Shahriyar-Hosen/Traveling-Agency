e1fdb8a7f6c674b0dafe7f098202fd9c287e16bd
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-09-04 20:28:15 +0600

    mobile search field done

--- src/Components/Home/Search.js
+++ src/Components/Home/Search.js
@@ -1,42 +1,41 @@
 import React from "react";
-import { GrLocation } from "react-icons/gr";
+import { VscLocation } from "react-icons/vsc";
 import { FaSearch } from "react-icons/fa";
 
-{
-  /*  */
-}
 const Search = () => {
   return (
-    <div>
-      <div className="flex justify-center items-center bg-primary">
-        <GrLocation /> <span>Find Your Holidays</span>
+    <div className="md:flex flex-col justify-center items-center  shadow-gray-900 mt-16">
+      <div className="flex justify-center items-center bg-primary text-white w-96 md:w-44  mx-auto h-24 rounded-lg rounded-b-none md:rounded-r-none gap-3 text-xl font-serif font-semibold pl-5">
+        <VscLocation className="text-3xl md:text-6xl text-white" />{" "}
+        <span>Find Your Holidays</span>
       </div>
-      
-      <form action="">
-        <input
-          type="text"
-          placeholder="Type here"
-          class="input input-bordered input-primary w-full max-w-xs"
-        />
-        <input
-          type="text"
-          placeholder="Type here"
-          class="input input-bordered input-primary w-full max-w-xs"
-        />
-        <input
-          type="text"
-          placeholder="Type here"
-          class="input input-bordered input-primary w-full max-w-xs"
-        />
-        <input
-          type="text"
-          placeholder="Type here"
-          class="input input-bordered input-primary w-full max-w-xs"
-        />
-        <button>
-          <FaSearch /> <span>Search Now</span>
-        </button>
-      </form>
+      <div className="w-96 mx-auto">
+        <form className="flex flex-col md:flex-row justify-center items-center gap-3 px-5 pb-5 rounded-lg shadow-xl shadow-gray-100">
+          <input
+            type="text"
+            placeholder="Destination"
+            class="w-full max-w-sm py-3 px-3 text-xs border outline-gray-400 rounded-md mt-3"
+          />
+          <input
+            type="text"
+            placeholder="dd/mm/yyy"
+            class="w-full max-w-sm py-3 px-3 text-xs border outline-gray-400 rounded-md"
+          />
+          <input
+            type="text"
+            placeholder="Travel Type"
+            class="w-full max-w-sm py-3 px-3 text-xs border outline-gray-400 rounded-md"
+          />
+          <input
+            type="text"
+            placeholder="Tour Duration"
+            class="w-full max-w-sm py-3 px-3 text-xs border outline-gray-400 rounded-md"
+          />
+          <button className="flex justify-center items-center gap-1 bg-primary w-full rounded-md py-3 text-white text-sm font-mono">
+            <FaSearch className="text-xs" /> <span>Search Now</span>
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
--- src/Components/Shared/Navbar.js
+++ src/Components/Shared/Navbar.js
@@ -64,7 +64,7 @@ const Navbar = () => {
         {/* Logo section */}
         <div class="navbar-start">
           <div class="dropdown">
-            <label tabindex="0" class="btn btn-ghost lg:hidden">
+            <label tabIndex="0" class="btn btn-ghost lg:hidden">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 class="h-5 w-5"
@@ -81,7 +81,7 @@ const Navbar = () => {
               </svg>
             </label>
             <ul
-              tabindex="0"
+              tabIndex="0"
               class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
             >
               {navMenu}
@@ -158,7 +158,7 @@ const Navbar = () => {
           <div class="flex justify-center items-center gap-2">
             <div class="dropdown dropdown-end">
               {/* img */}
-              <label tabindex="0" class="btn btn-ghost btn-circle avatar">
+              <label tabIndex="0" class="btn btn-ghost btn-circle avatar">
                 <div class="w-10 rounded-full">
                   <img src="https://placeimg.com/192/192/people" alt="" />
                 </div>
@@ -166,7 +166,7 @@ const Navbar = () => {
 
               {/* Profile Section Menu*/}
               <ul
-                tabindex="0"
+                tabIndex="0"
                 class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
               >
                 {profileMenu}
