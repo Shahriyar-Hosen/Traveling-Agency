09c2a20bb2e3fcfa21970923e13836b1de65dbdf
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-09-05 20:16:41 +0600

    Top Destinations section setup done

---
+++ src/Components/Home/TopDestinations.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const TopDestinations = () => {
+  return (
+    <div>
+      <div className="flex justify-center items-center flex-col gap-5  text-center w-3/4 mx-auto">
+        <h5 className="font-serif text-secondary font-semibold text-lg mt-16">
+          Top Destinations
+        </h5>
+        <h1 className="font-serif text-3xl md:text-4xl text-primary ">
+          <span className="text-neutral">Explore</span> Top Destinations
+        </h1>
+        <p className="max-w-xl">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore.
+        </p>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default TopDestinations;
--- src/Page/Home.js
+++ src/Page/Home.js
@@ -3,12 +3,14 @@ import BestYear from "../Components/Home/BestYear";
 import CoreFeatures from "../Components/Home/CoreFeatures";
 import Header from "../Components/Home/Header";
 import Places from "../Components/Home/Places";
+import TopDestinations from "../Components/Home/TopDestinations";
 
 const Home = () => {
   return (
     <div>
       <Header />
       <CoreFeatures />
+      <TopDestinations />
       <Places />
       <BestYear />
     </div>
