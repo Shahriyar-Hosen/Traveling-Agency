e9b90ea66d9f9fe3aedf9424ed58e8ce0fa6e721
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-09-08 08:43:47 +0600

    TourPackages card style done

--- src/Components/TourPackages/TourPackages.js
+++ src/Components/TourPackages/TourPackages.js
@@ -5,7 +5,7 @@ import bgImg from "../../Assets/background/shape8.png";
 const TourPackages = () => {
   return (
     <div
-      className="min-h-screen my-10"
+      className="min-h-screen my-10 mx-10"
       style={{
         background: `url(${bgImg})`,
         backgroundSize: "cover",
@@ -33,7 +33,69 @@ const TourPackages = () => {
           </button>{" "}
         </div>
       </div>
-      <div></div>
+      <div className="flex justify-center mt-24">
+        <div className="card card-compact w-96 bg-base-100 shadow-xl">
+          <figure>
+            <img src="https://i.ibb.co/jrGj182/trending3.jpg" alt="Shoes" />
+          </figure>
+          <div className="card-body">
+            <h2 className="text-primary text-lg  font-serif font-semibold ">
+              Greece
+            </h2>
+            <h1 className="text-2xl font-serif font-semibold hover:text-primary">
+              Santorini, Oia
+            </h1>
+            <span className="flex justify-start items-center">
+              <div className="rating rating-sm">
+                <input
+                  type="radio"
+                  name="rating-2"
+                  className="mask mask-star-2 bg-orange-400"
+                />
+                <input
+                  type="radio"
+                  name="rating-2"
+                  className="mask mask-star-2 bg-orange-400 "
+                  checked
+                />
+                <input
+                  type="radio"
+                  name="rating-2"
+                  className="mask mask-star-2 bg-orange-400"
+                  checked
+                />
+                <input
+                  type="radio"
+                  name="rating-2"
+                  className="mask mask-star-2 bg-orange-400"
+                  checked
+                />
+                <input
+                  type="radio"
+                  name="rating-2"
+                  className="mask mask-star-2 bg-orange-400"
+                  checked
+                />
+              </div>
+              <span className="px-1">(38)</span>
+            </span>
+            <p className="text-base py-2">
+              Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum
+            </p>
+            <div className="card-actions justify-start border-t pt-3">
+              <div className="flex justify-center items-center gap-2">
+                <span className="text-primary text-xl font-bold">$180.00</span>
+
+                <span className="text-lg text-gray-500 font-semibold">
+                  {" "}
+                  | Per person
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
