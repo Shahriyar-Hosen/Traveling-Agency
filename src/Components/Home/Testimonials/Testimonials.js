b927628eef611dcd1de95f6f57f90146e39bde18
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-09-08 17:44:48 +0600

    SideCard style full done

--- src/Components/Home/Testimonials/Testimonials.js
+++ src/Components/Home/Testimonials/Testimonials.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { FaQuoteLeft, FaRegCalendarAlt } from "react-icons/fa";
+import { FaQuoteLeft } from "react-icons/fa";
+import SideCard from "../../theme/SideCard";
 
 const Testimonials = () => {
   return (
@@ -60,74 +61,8 @@ const Testimonials = () => {
         </div>
       </div>
       {/* testimonial card */}
-      <div className="card lg:card-side bg-base-100 shadow-xl lg:w-[650px]">
-        <figure>
-          <img
-            className="lg:max-w-[300px] w-full h-[300px] lg:h-full"
-            src="https://placeimg.com/400/400/arch"
-            alt="Album"
-          />
-        </figure>
-        <div className="card-body text-start">
-          <h2 className="text-primary text-lg  font-serif font-semibold ">
-            Greece
-          </h2>
-          <h1 className="text-2xl font-serif font-semibold hover:text-primary">
-            Santorini, Oia
-          </h1>
-          <span className="flex justify-start items-center">
-            <div className="rating rating-sm">
-              <input
-                type="radio"
-                name="rating-2"
-                className="mask mask-star-2 bg-secondary"
-              />
-              <input
-                type="radio"
-                name="rating-2"
-                className="mask mask-star-2 bg-secondary "
-                checked
-              />
-              <input
-                type="radio"
-                name="rating-2"
-                className="mask mask-star-2 bg-secondary"
-                checked
-              />
-              <input
-                type="radio"
-                name="rating-2"
-                className="mask mask-star-2 bg-secondary"
-                checked
-              />
-              <input
-                type="radio"
-                name="rating-2"
-                className="mask mask-star-2 bg-secondary"
-                checked
-              />
-            </div>
-            <span className="px-1">(38)</span>
-          </span>
-          <p className="text-base pt-2">
-            Duis aute irure dolor in reprehenderit in voluptate velit esse
-            cillum
-          </p>
-          <p className="text-primary font-semibold flex justify-start items-center gap-2">
-            <FaRegCalendarAlt /> 9 Days Tours
-          </p>
-          <div className="card-actions justify-start border-t pt-3">
-            <div className="flex justify-center items-center gap-2">
-              <span className="text-primary text-xl font-bold">$180.00</span>
-
-              <span className="text-lg text-gray-500 font-semibold">
-                {" "}
-                | Per person
-              </span>
-            </div>
-          </div>
-        </div>
-      </div>
+      {/* { h2, h1, rating, details, time, price } */}
+      <SideCard img="" h2="" h1="" rating="" details="" time="" price="" />
     </section>
   );
 };
