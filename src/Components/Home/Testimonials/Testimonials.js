5d57bb6fa721a4627be1b939e10df9d9b27f26df
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-09-10 19:12:13 +0600

    Testimonials style done

--- src/Components/Home/Testimonials/Testimonials.js
+++ src/Components/Home/Testimonials/Testimonials.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { FaQuoteLeft } from "react-icons/fa";
+import Content from "../../theme/Content";
 import SideCard from "../../theme/SideCard";
 
 const Testimonials = () => {
@@ -11,57 +12,59 @@ const Testimonials = () => {
           backgroundImage: "url(https://i.ibb.co/6bMmXfm/bg.png)",
         }}
       >
-        <div className="hero-overlay bg-opacity-25"></div>
-        <div className="hero-content  text-neutral-content  py-14">
-          <div className="flex flex-col lg:flex-row lg:px-12 px-5 my-10 gap-5">
-            {/* Header */}
-            <div className="w-full lg:w-7/12 flex flex-col justify-start items-start gap-5">
-              <span className="text-2xl font-serif font-bold text-secondary">
-                Our Testimonials
-              </span>
-              <h2 className="text-4xl md:text-5xl font-bold text-white font-serif">
-                Good Reviews By <br />{" "}
-                <span className="text-primary">Clients</span>
-              </h2>
-              <p className="text-white">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore.
-              </p>
-            </div>
-            {/* testimonial section */}
-            <div className="w-full lg:w-8/12 ">
-              <p className="text-white text-lg flex justify-start gap-2">
-                <span className="text-primary text-3xl">
-                  <FaQuoteLeft />
+        <Content>
+          <div className="hero-overlay bg-opacity-25"></div>
+          <div className="hero-content  text-neutral-content  py-14">
+            <div className="flex flex-col lg:flex-row lg:px-12 px-5 my-10 gap-5">
+              {/* Header */}
+              <div className="w-full lg:w-7/12 flex flex-col justify-start items-start gap-5">
+                <span className="text-2xl font-serif font-bold text-secondary">
+                  Our Testimonials
                 </span>
-                <i className="text-xl">
-                  Lorem Ipsum is simply dummy text of the printing andypesetting
-                  industry. Lorem ipsum a simple Lorem Ipsum has been the
-                  industry's standard dummy hic et quidem. Dignissimos maxime
-                  velit unde inventore quasi vero dolorem.
-                </i>
-              </p>
-              <div className="flex justify-center lg:justify-start items-center gap-5 py-10 ">
-                <div className="avatar lg:ml-7">
-                  <div className="w-24 rounded-full">
-                    <img src="https://placeimg.com/192/192/people" alt="" />
-                  </div>
-                </div>
-                <div>
-                  <h5 className="text-secondary font-serif text-xl font-bold">
-                    Kelton Mick
-                  </h5>
-                  <span className="text-white text-md font-semibold">
-                    Manager
+                <h2 className="text-4xl md:text-5xl font-bold text-white font-serif">
+                  Good Reviews By <br />{" "}
+                  <span className="text-primary">Clients</span>
+                </h2>
+                <p className="text-white">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore.
+                </p>
+              </div>
+              {/* testimonial section */}
+              <div className="w-full lg:w-8/12 ">
+                <p className="text-white text-lg flex justify-start gap-2">
+                  <span className="text-primary text-3xl">
+                    <FaQuoteLeft />
                   </span>
+                  <i className="text-xl">
+                    Lorem Ipsum is simply dummy text of the printing
+                    andypesetting industry. Lorem ipsum a simple Lorem Ipsum has
+                    been the industry's standard dummy hic et quidem.
+                    Dignissimos maxime velit unde inventore quasi vero dolorem.
+                  </i>
+                </p>
+                <div className="flex justify-center lg:justify-start items-center gap-5 py-10 ">
+                  <div className="avatar lg:ml-7">
+                    <div className="w-24 rounded-full">
+                      <img src="https://placeimg.com/192/192/people" alt="" />
+                    </div>
+                  </div>
+                  <div>
+                    <h5 className="text-secondary font-serif text-xl font-bold">
+                      Kelton Mick
+                    </h5>
+                    <span className="text-white text-md font-semibold">
+                      Manager
+                    </span>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
+        </Content>
       </div>
       {/* testimonial card */}
-      <div className="-mt-20 mx-5 flex flex-col lg:flex-row justify-center items-center gap-5">
+      <Content className="-mt-20 flex flex-col lg:flex-row justify-center items-center gap-5">
         <SideCard
           img="https://i.ibb.co/zGzhCkz/trending2.jpg"
           h2="Greece"
@@ -82,7 +85,7 @@ const Testimonials = () => {
           price="185.00"
           offers="30"
         />
-      </div>
+      </Content>
     </section>
   );
 };
