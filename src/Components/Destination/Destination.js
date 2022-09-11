46f8983139b3ff72d82d00d50f6e17426aecc259
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-09-11 19:13:21 +0600

    Average Reviews done

--- src/Components/Destination/Destination.js
+++ src/Components/Destination/Destination.js
@@ -48,6 +48,7 @@ const Destination = () => {
                 <span>(1,186 Reviews)</span>
               </p>
             </div>
+            {/* Large Image */}
             <figure className="">
               <img
                 className="w-full rounded-lg"
@@ -55,6 +56,7 @@ const Destination = () => {
                 alt=""
               />
             </figure>
+            {/* Description */}
             <div className="my-10">
               <h1 className="text-2xl font-semibold font-serif">Description</h1>
               <p className="mt-5">
@@ -96,6 +98,7 @@ const Destination = () => {
                 </div>
               </div>
             </div>
+            {/* Gallery */}
             <div className="my-10">
               <h1 className="text-2xl font-semibold font-serif pb-3">
                 Gallery
@@ -118,6 +121,7 @@ const Destination = () => {
                 </figure>
               </div>
             </div>
+            {/* Map */}
             <div className="my-10">
               <h1 className="text-2xl font-semibold font-serif pb-3">Map</h1>
               <div class="map rounded overflow-hidden">
@@ -126,13 +130,67 @@ const Destination = () => {
                     className=" rounded overflow-hidden w-full"
                     height="400"
                     title="google maps"
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d116834.05517529047!2d90.27575512876744!3d23.780727221499042!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b8b087026b81%3A0x8fa563bbdd5904c2!2sDhaka%2C%20Bangladesh!5e0!3m2!1sen!2sus!4v1662898823726!5m2!1sen!2sus"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d116834.05517529047!2d90.27575512876744!3d23.780727221499042!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b8b087026b81%3A0x8fa723bbdd5904c2!2sDhaka%2C%20Bangladesh!5e0!3m2!1sen!2sus!4v1662898823726!5m2!1sen!2sus"
                     loading="lazy"
                     referrerpolicy="no-referrer-when-downgrade"
                   ></iframe>
                 </div>
               </div>
               <div className="flex md:flex-row flex-col justify-start items-center gap-5"></div>
+            </div>
+            {/* Average Reviews */}
+            <div className="my-10 flex flex-col md:flex-row gap-10">
+              <div className="md:w-64 bg-[#17233e] text-white p-8 rounded-lg  text-center flex flex-col gap-4">
+                <span className="text-5xl font-serif">2.2/5</span>{" "}
+                <p>
+                  <q className="text-xl font-serif">
+                    Feel So Much Worst Than Thinking
+                  </q>{" "}
+                </p>
+                <span>From 40 Reviews</span>
+              </div>
+              <div className="flex flex-col gap-1 justify-center w-full mr-10">
+                <span className="text-lg font-semibold">
+                  <h1>Cleanliness</h1>
+                  <progress
+                    className="progress progress-primary w-full"
+                    value="45"
+                    max="100"
+                  ></progress>
+                </span>
+                <span className="text-lg font-semibold">
+                  <h1>Facilities</h1>
+                  <progress
+                    className="progress progress-primary w-full"
+                    value="40"
+                    max="100"
+                  ></progress>
+                </span>
+                <span className="text-lg font-semibold">
+                  <h1>Value for money</h1>
+                  <progress
+                    className="progress progress-primary w-full"
+                    value="30"
+                    max="100"
+                  ></progress>
+                </span>
+                <span className="text-lg font-semibold">
+                  <h1>Service</h1>
+                  <progress
+                    className="progress progress-primary w-full"
+                    value="45"
+                    max="100"
+                  ></progress>
+                </span>
+                <span className="text-lg font-semibold">
+                  <h1>Location</h1>
+                  <progress
+                    className="progress progress-primary w-full"
+                    value="20"
+                    max="100"
+                  ></progress>
+                </span>
+              </div>
             </div>
           </div>
         </Content>
