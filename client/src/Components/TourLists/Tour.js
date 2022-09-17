0b533491876adbbb96583aac9b1a9f06b1bbaa7d
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-09-17 23:10:13 +0600

    Gallery

--- client/src/Components/TourLists/Tour.js
+++ client/src/Components/TourLists/Tour.js
@@ -4,6 +4,7 @@ import LoveGoing from "../Shared/LoveGoing";
 import Partners from "../Shared/Partners";
 import Content from "../theme/Content";
 import Header from "../theme/Header";
+import Gallery from "./Gallery";
 import TourHeader from "./TourHeader";
 
 const Tour = () => {
@@ -15,32 +16,7 @@ const Tour = () => {
         {/* Header */}
         <TourHeader />
         {/* Gallery */}
-        <div className="my-10">
-          {/* <h1 className="text-2xl font-semibold font-serif pb-3">Gallery</h1> */}
-          <div className="flex md:flex-row flex-col flex-wrap lg:flex-nowrap justify-center items-center gap-5">
-            <figure className="w-full">
-              <img
-                className="rounded-lg w-full max-w-"
-                src="https://i.ibb.co/r77YXNS/trending1.jpg"
-                alt=""
-              />
-            </figure>
-            <figure className=" w-full">
-              <img
-                className="rounded-lg w-full"
-                src="https://i.ibb.co/zGzhCkz/trending2.jpg"
-                alt=""
-              />
-            </figure>
-            <figure className=" w-full">
-              <img
-                className="rounded-lg w-full"
-                src="https://i.ibb.co/jrGj182/trending3.jpg"
-                alt=""
-              />
-            </figure>
-          </div>
-        </div>
+        <Gallery />
         {/* Description */}
         <div className="my-10">
           <h1 className="text-2xl font-semibold font-serif">Description</h1>
