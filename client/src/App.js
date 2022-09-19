4e821152fec5646e60578e0c7d74d3907982b035
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-09-19 15:28:22 +0600

    all added

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,19 @@
-import { Route, Routes } from "react-router-dom";
-import "./App.css";
-import Login from "./Components/auth/Login";
-import Register from "./Components/auth/Register";
-import Destination from "./Components/Destination/Destination";
-import Footer from "./Components/Shared/Footer";
-import Navbar from "./Components/Shared/Navbar";
-import Tour from "./Components/TourLists/Tour";
-import Destinations from "./Page/Destinations";
-import Home from "./Page/Home.js";
-import Hotels from "./Page/Hotels";
-import Tours from "./Page/Tours";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Hotel from "./pages/hotel/Hotel";
+import List from "./pages/list/List";
+import Login from "./pages/login/Login";
 
 function App() {
   return (
-    <main>
-      <Navbar />
+    <BrowserRouter>
       <Routes>
-        {/* Public Page */}
         <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/hotels" element={<List />} />
+        <Route path="/hotels/:id" element={<Hotel />} />
         <Route path="/login" element={<Login />} />
-
-        {/* Privet Page */}
-        <Route path="destinations" element={<Destinations />} />
-        <Route path="destinations/:destinationId" element={<Destination />} />
-        <Route path="tours" element={<Tours />} />
-        <Route path="tours/:tourId" element={<Tour />} />
-        <Route path="hotels" element={<Hotels />} />
       </Routes>
-      <Footer />
-    </main>
+    </BrowserRouter>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-
-// eslint-disable-next-line
-import "swiper/css/bundle";
+import { AuthContextProvider } from "./context/AuthContext";
+import { SearchContextProvider } from "./context/SearchContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <AuthContextProvider>
+      <SearchContextProvider>
+        <App />
+      </SearchContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
