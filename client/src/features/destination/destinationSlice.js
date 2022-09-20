a1b7c407c47f81f60ec8aab6cde604d5986820f2
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-09-20 20:41:16 +0600

    destination Api & Slice setup done

---
+++ client/src/features/destination/destinationSlice.js
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {};
+
+const destinationSlice = createSlice({
+  name: "destination",
+  initialState,
+  reducers: {},
+});
+
+export const {} = destinationSlice.actions;
+export default destinationSlice.reducer;
