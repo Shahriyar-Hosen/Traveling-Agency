4b2125c9818d0ba1646e74eab78f331e0d6d48a2
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-09-20 18:59:23 +0600

    Redux Toolkit install & setup done

--- client/src/index.js
+++ client/src/index.js
@@ -7,11 +7,17 @@ import { BrowserRouter } from "react-router-dom";
 // eslint-disable-next-line
 import "swiper/css/bundle";
 
+// Redux Toolkit
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
