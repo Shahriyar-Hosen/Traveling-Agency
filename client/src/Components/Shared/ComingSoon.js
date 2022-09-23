b93a22e53784440effc7d627a4e79d46b3a6ee4d
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-09-23 11:10:52 +0600

    Coming Soon full done

--- client/src/Components/Shared/ComingSoon.js
+++ client/src/Components/Shared/ComingSoon.js
@@ -1,15 +1,19 @@
 import React from "react";
+import logo from "../../Assets/log/logo.png";
+import Content from "../theme/Content";
 
 const ComingSoon = () => {
   return (
-    <section
-      style={{
-        background: `url(https://res.cloudinary.com/dev-shahriyar/image/upload/v1663907867/Pages/Coming-soon-white_bw89uh.jpg)`,
-        backgroundSize: "cover",
-      }}
-      className="w-full min-h-screen"
-      
-    ></section>
+    <Content className="relative">
+      <img
+        className="w-full min-h-screen max-h-screen"
+        src="https://res.cloudinary.com/dev-shahriyar/image/upload/v1663909751/Pages/Coming-soon-yello_zilsgl.jpg"
+        alt=""
+      />
+      <div className="absolute top-28 left-0 right-0 z-10 flex justify-center items-center">
+        <img src={logo} alt="" />
+      </div>
+    </Content>
   );
 };
 
