daae88531d52f054281ec000f73de94c5de3d7b0
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-10-19 15:26:05 +0600

    socialLogin try out

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -3,6 +3,7 @@ import bcrypt from "bcryptjs";
 import { createError } from "../utils/error.js";
 import jwt from "jsonwebtoken";
 
+// Register
 export const register = async (req, res, next) => {
   try {
     const salt = bcrypt.genSaltSync(10);
@@ -31,7 +32,44 @@ export const register = async (req, res, next) => {
     next(err);
   }
 };
+
+// Login 
 export const login = async (req, res, next) => {
+  try {
+
+    const user = await User.findOne({ username: req.body.username });
+
+    if (!user) return next(createError(404, "User not found!"));
+
+    const isPasswordCorrect = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+
+    if (!isPasswordCorrect)
+      return next(createError(400, "Wrong password or username!"));
+
+    const token = jwt.sign(
+      { id: user._id, isAdmin: user.isAdmin },
+      process.env.JWT
+    );
+
+    const { password, isAdmin, ...otherDetails } = user._doc;
+
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json({ user: { ...otherDetails }, accessToken: token, isAdmin });
+
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Social Login
+export const socialLogin = async (req, res, next) => {
   try {
 
     const user = await User.findOne({ username: req.body.username });
