e6c2c7f90a8077670281562f2b9b817b833701df
shahriyarhosen <me.shahriyarhosen@gmail.com>
2022-10-19 15:29:14 +0600

    social Login root added

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { login, register } from "../controllers/auth.js";
+import { login, register, socialLogin } from "../controllers/auth.js";
 
 const router = express.Router();
 
 router.post("/register", register)
 router.post("/login", login)
+router.post("/social", socialLogin)
 
 export default router
