e23b623863169359bb8ffbec65efa789629bac2e
Shahriyar Hosen <96829173+shahriyarhosen@users.noreply.github.com>
2022-11-04 16:06:48 +0600

    Delete client directory

--- client/src/features/tours/toursApi.js
+++
@@ -1,9 +0,0 @@
-import { apiSlice } from "../api/apiSlice";
-
-export const toursApi = apiSlice.injectEndpoints({
-  endpoints: (builder) => ({
-    // endpoints here
-  }),
-});
-
-export const {} = toursApi;
