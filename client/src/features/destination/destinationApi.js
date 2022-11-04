399c4c9daad498f827df58b561518bef73c78f06
Shahriyar Hosen <96829173+shahriyarhosen@users.noreply.github.com>
2022-11-04 15:47:29 +0600

    Delete client directory

--- client/src/features/destination/destinationApi.js
+++
@@ -1,9 +0,0 @@
-import { apiSlice } from "../api/apiSlice";
-
-export const destinationApi = apiSlice.injectEndpoints({
-  endpoints: (builder) => ({
-    // endpoints here
-  }),
-});
-
-export const {} = destinationApi;
